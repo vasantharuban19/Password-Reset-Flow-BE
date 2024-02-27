1dd81838833800503858e830381fd46678d2f298
vasantharuban19 <vasanthruban1920@gmail.com>
2024-02-27 18:34:18 +0530

    updated

--- src/models/index.js
+++ src/models/index.js
@@ -2,6 +2,6 @@ import mongoose from "mongoose";
 import dotenv from 'dotenv';
 dotenv.config()
 
-mongoose.connect(`${process.env.DB_URL}`)
+mongoose.connect(`${process.env.DB_URL}`,{dbName:process.env.DB_NAME})
 // console.log(process.env.DB_NAME);
 export default mongoose
